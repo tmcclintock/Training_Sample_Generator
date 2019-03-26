d64c935a2ad5ccbf361b05df78af87d803ec9ada
tmcclintock <tmcclintock89@gmail.com>
2019-03-26 09:55:05 -0400

    More tests.

--- tests/test_sampler.py
+++ tests/test_sampler.py
@@ -16,16 +16,25 @@ def test_sampler_builds(): #smoke test
 def test_exceptions():
     R = np.diag([1., 1.])
     chain = np.random.randn(2, 1000)
-    with pytest.raises(Exception):
+    with npt.assert_raises(Exception):
         s = sg.SampleGenerator()
-    with pytest.raises(Exception):
+    with npt.assert_raises(Exception):
         s = sg.SampleGenerator(chain=chain, scale=0)
-    with pytest.raises(Exception):
+    with npt.assert_raises(Exception):
         s = sg.SampleGenerator(chain=chain, scale=-1)
-    with pytest.raises(Exception):
+    with npt.assert_raises(Exception):
         s = sg.SampleGenerator(covariance=R, scale=0)
-    with pytest.raises(Exception):
+    with npt.assert_raises(Exception):
         s = sg.SampleGenerator(covariance=R, scale=-1)
-    with pytest.raises(Exception):
+    with npt.assert_raises(Exception):
         s = sg.SampleGenerator(covariance=R, scale=99)
+    with npt.assert_raises(Exception):
+        s = sg.SampleGenerator(chain=chain, covariance=R)
     return
+
+def test_samples():
+    chain = np.random.randn(2, 1000)
+    s = sg.SampleGenerator(chain=chain)
+    N = 100 #N_samples
+    samp = s.generate_flat_samples(N)
+    
